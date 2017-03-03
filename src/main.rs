aeb2d224f0db51407b0d703e0aae21d071e7947f
Ben Burdette <bburdette@gmail.com>
2017-03-03 08:28:05 -0700

    working again with new oscpad

--- src/main.rs
+++ src/main.rs
@@ -10,6 +10,7 @@ use std::sync::mpsc;
 use std::thread;
 use std::str::FromStr;
 use std::cmp::min;
+use std::time::Duration;
 
 extern crate portaudio;
 
@@ -18,6 +19,15 @@ use portaudio::{stream, hostapi, device};
 
 extern crate tinyosc;
 use tinyosc as osc;
+
+const CHANNELS: i32 = 2;
+const NUM_SECONDS: i32 = 5;
+const SAMPLE_RATE: f64 = 44100.0;
+// const FRAMES_PER_BUFFER: u32 = 64;
+// const FRAMES_PER_BUFFER: u32 = 2048;
+const FRAMES_PER_BUFFER: u32 = 4096;
+const IN_DEVICE: u32 = 0;
+const OUT_DEVICE: u32 = 0;
 
 extern {
   pub fn fraust_init(samplerate: i32);
@@ -43,11 +53,6 @@ pub struct SliderEvt {
 , position: f32
 }
 
-const CHANNELS: i32 = 2;
-const NUM_SECONDS: i32 = 5;
-const SAMPLE_RATE: f64 = 44100.0;
-// const FRAMES_PER_BUFFER: u32 = 64;
-const FRAMES_PER_BUFFER: u32 = 2048;
 
 fn main()
 {
@@ -65,9 +70,28 @@ fn print_devs()
         match portaudio::device::get_info(i)
         {
             None => {},
-            Some(info) => println!("{}: {}", i, info.name),
+            // Some(info) => println!("{}: {:?}", i, info),
+            Some(info) => { 
+              println!("-------------------------------------------");
+              println!("Device {}: ", i);
+              print_device(info);
+              },
         }
     }
+    println!("-------------------------------------------");
+}
+
+fn print_device(info: device::DeviceInfo)
+{
+  println!("name: {}", info.name); 
+  println!("host_api: {}", info.host_api);
+  println!("max_input_channels: {}", info.max_input_channels);
+  println!("max_output_channels: {}", info.max_output_channels);
+  println!("default_low_input_latency: {}", info.default_low_input_latency.num_milliseconds());
+  println!("default_low_output_latency: {}", info.default_low_output_latency.num_milliseconds());
+  println!("default_high_input_latency: {}", info.default_high_input_latency.num_milliseconds());
+  println!("default_high_output_latency: {}", info.default_high_output_latency.num_milliseconds());
+  println!("default_sample_rate: {}", info.default_sample_rate);
 }
 
 fn callback_demo()
@@ -118,11 +142,11 @@ fn callback_demo()
           Ok(se) => {
             match se.what { 
               SeWhat::Millisecond => { 
-                  // println!("setting vol to 0.3!");
+                   println!("setting vol to {}!", se.position);
                    unsafe { fraust_setval(millisecond.as_ptr(), se.position); }
                 }
               SeWhat::Feedback => { 
-                  // println!("setting vol to 0.001!");
+                  println!("setting vol to {}!", se.position);
                   unsafe { fraust_setval(feedback.as_ptr(), se.position); }
                 }
             }
@@ -142,7 +166,7 @@ fn callback_demo()
     // ---------------------------------------------
     // start portaudio 
     // ---------------------------------------------
-    let in_idx = 2;
+    let in_idx = IN_DEVICE;
 	/*
     let in_idx = match device::get_default_input_index()
     {
@@ -151,12 +175,15 @@ fn callback_demo()
     };*/
     let in_lat = match device::get_info(in_idx)
     {
-        None => return,
+        None => {
+          println!("input device not found for index: {}", in_idx);
+          return
+          }
         Some(d) => d.default_low_input_latency,
     };
     let inparams = stream::StreamParameters { device: in_idx, channel_count: 1, suggested_latency: in_lat, data: 0f32 };
 
-    let out_idx = 2;
+    let out_idx = OUT_DEVICE;
     /* let out_idx = match device::get_default_output_index()
     {
         Some(o) => o,
@@ -164,11 +191,13 @@ fn callback_demo()
     }; */
     let out_lat = match device::get_info(out_idx)
     {
-        None => return,
+        None => {
+          println!("output device not found for index: {}", out_idx);
+          return
+          }
         Some(d) => d.default_low_output_latency,
     };
     let outparams = stream::StreamParameters { device: out_idx, channel_count: 2, suggested_latency: out_lat, data: 0f32 };
-
 
     let mut stream = match stream::Stream::open(Some(inparams), 
                                                 Some(outparams), 
@@ -211,6 +240,55 @@ fn callback_demo()
     };
 }
 
+fn find_location(args: &Vec<osc::Argument>) -> Option<f32> {
+  let mut arg_iter = args.into_iter();
+  let mut arg = arg_iter.next();
+
+  let mut ret = None; // Option<f32>;
+
+  while arg.is_some()
+  {
+    match arg
+    {
+    Some(&osc::Argument::s("location")) => {
+      arg = arg_iter.next();
+      match arg {
+        Some(&osc::Argument::f(loc)) => {
+          ret = Some(loc);
+          break;
+          },
+        _ => {
+          continue;
+        }
+      }
+    }
+    _ => {
+      arg = arg_iter.next();
+        continue;
+    }
+    }
+  }
+  ret
+}
+
+fn find_etype(args: &Vec<osc::Argument>) -> Option<SeType> {
+  if args.len() >= 1 {
+    match &args[0] {
+      &osc::Argument::s(etype) => {
+        match etype { 
+        "pressed" => Some(SeType::SliderPress),
+        "unpressed" => Some(SeType::SliderUnpress),
+        // "moved" => Some(SeType::SliderMove),
+        _ => Some(SeType::SliderMove),
+        }
+	}
+      _ => Some(SeType::SliderMove),
+      // _ => None,
+     }
+   }
+   else { None }
+}
+ 
 fn oscthread(oscrecvip: SocketAddr, sender: mpsc::Sender<SliderEvt>) -> Result<String, Error> { 
   let socket = try!(UdpSocket::bind(oscrecvip));
   let mut buf = [0; 1000];
@@ -226,9 +304,37 @@ fn oscthread(oscrecvip: SocketAddr, sender: mpsc::Sender<SliderEvt>) -> Result<S
         },
       };
 
-    // println!("message received {} {:?}", inmsg.path, inmsg.arguments );
+    println!("message received {} {:?}", inmsg.path, inmsg.arguments );
     match inmsg {
       osc::Message { path: ref path, arguments: ref args } => {
+        let what = match path { 
+          &"millisecond" => Some(SeWhat::Millisecond),
+          &"feedback" => Some(SeWhat::Feedback),
+          _ => None,
+          };
+
+        println!(" what and stuff: {:?}, {:?},  {:?}",  
+		what.is_none(), find_etype(args).is_none(), find_location(args).is_none());
+        match (what, find_etype(args), find_location(args)) { 
+          (Some(what), Some(sevt), Some(pos)) => { 
+            let amt = match what { 
+              SeWhat::Millisecond => pos * 500.0,
+              SeWhat::Feedback => pos * 100.0,
+              };
+              
+            let se = SliderEvt{ evttype: sevt, what: what, position: amt };
+            sender.send(se);
+            },
+          _ => {},
+	  }
+	}
+     };
+   }
+ }
+
+/*         match (find_location(args)) {
+          Some(loc) => 
+
         if args.len() > 1 {
           match (&args[0], &args[1]) {
             (&osc::Argument::s(etype), &osc::Argument::f(pos)) => {
@@ -271,5 +377,6 @@ fn oscthread(oscrecvip: SocketAddr, sender: mpsc::Sender<SliderEvt>) -> Result<S
   // drop(socket); // close the socket
   // Ok(String::from("meh"))
 }
+*/
 
 
